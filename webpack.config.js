d5f70f2be1943a7b3ca47879459c635d1cb04c0b
romanella <romanella@dls.rutgers.edu>
2018-12-17 14:10:10 -0500

    Remove uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
     entry: './src/index.ts',
@@ -9,26 +8,20 @@ module.exports = {
     },
     module: {
         rules: [{
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        },
+        {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }
         ]
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js']
-    },
-    plugins: [
-        new UglifyJsPlugin({
-            test: /\.js($|\?)/i,
-            exclude: /node_modules/
-        })
-      ]
+    }
 };
